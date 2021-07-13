52229a55b7b3667bdf97e47905b774f3d51f4de6
spirallab <labspiral@gmail.com>
2021-07-13 15:37:44 +0900

    fixed) demo 프로그램(markshapes) 코드 보강

--- demos/markshapes/Program.cs
+++ demos/markshapes/Program.cs
@@ -27,11 +27,15 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Numerics;
+using System.Threading;
 
 namespace SpiralLab.Sirius
 {
     class Program
     {
+
+        static Stopwatch timer;
+
         static void Main(string[] args)
         {
             SpiralLab.Core.Initialize();
@@ -48,7 +52,7 @@ namespace SpiralLab.Sirius
             var correctionFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "correction", "cor_1to1.ct5");
             rtc.Initialize(kfactor, LaserMode.Yag1, correctionFile);    // 스캐너 보정 파일 지정 : correction file
             rtc.CtlFrequency(50 * 1000, 2); // laser frequency : 50KHz, pulse width : 2usec
-            rtc.CtlSpeed(100, 100); // default jump and mark speed : 100mm/s
+            rtc.CtlSpeed(500, 500); // default jump and mark speed : 500mm/s
             rtc.CtlDelay(10, 100, 200, 200, 0); // scanner and laser delays
             #endregion
 
@@ -61,22 +65,21 @@ namespace SpiralLab.Sirius
             {
                 Console.WriteLine($"{Environment.NewLine}");
                 Console.WriteLine("Testcase for spirallab.sirius. powered by labspiral@gmail.com (http://spirallab.co.kr)");
-                Console.WriteLine($"{Environment.NewLine}");
+                Console.WriteLine($"----------------------------------------------------------------------------------------");
                 Console.WriteLine("'S' : get status");
                 Console.WriteLine("'L' : draw line");
                 Console.WriteLine("'C' : draw circle");
                 Console.WriteLine("'R' : draw rectangle");
                 Console.WriteLine("'D' : draw circle with dots");
                 Console.WriteLine("'P' : draw square area with pixel operation");
+                Console.WriteLine("'H' : draw heavy and slow job with thread");
+                Console.WriteLine("'A' : abort to mark and finish the thread");
                 Console.WriteLine("'Q' : quit");
-                Console.WriteLine($"{Environment.NewLine}");
-                Console.Write("select your target : ");
+                Console.Write("Select your target : ");
                 key = Console.ReadKey(false);
                 Console.WriteLine($"{Environment.NewLine}");
                 if (key.Key == ConsoleKey.Q)
                     break;
-                Console.WriteLine($"{Environment.NewLine}");
-                var timer = Stopwatch.StartNew();
                 switch (key.Key)
                 {
                     case ConsoleKey.S:  //RTC의 상태 확인
@@ -90,29 +93,34 @@ namespace SpiralLab.Sirius
                             Console.WriteLine($"Rtc status has an error");                        
                         break;
                     case ConsoleKey.L:  // 선 모양 가공
-                        Console.WriteLine("WARNING !!! LASER IS BUSY ...");
                         DrawLine(laser, rtc, -10,-10, 10, 10);
                         break;
                     case ConsoleKey.C:  // 원 모양 가공
-                        Console.WriteLine("WARNING !!! LASER IS BUSY ...");
                         DrawCircle(laser, rtc, 10);
                         break;
                     case ConsoleKey.R:  // 사각형 모양 가공
-                        Console.WriteLine("WARNING !!! LASER IS BUSY ...");
                         DrawRectangle(laser, rtc, 10, 10);
                         break;
                     case ConsoleKey.D:  //점으로 이루어진 원 모양 가공
-                        Console.WriteLine("WARNING !!! LASER IS BUSY ...");
                         DrawCircleWithDots(laser, rtc, 10, 1.0f);
                         break;
                     case ConsoleKey.P:  // 픽셀 이루어진 사각 모양 가공
-                        Console.WriteLine("WARNING !!! LASER IS BUSY ...");
                         DrawSquareAreaWithPixels(laser, rtc, 10, 0.2f);
                         break;
+                    case ConsoleKey.H:  // 시간이 오래 걸리는 모양을 가공 (별도 가공 쓰레드 생성하여 처리)
+                        DrawTooHeavyAndSlowJob(laser, rtc);
+                        break;
+                    case ConsoleKey.A:  // 강제 가공 중지
+                        StopMarkAndReset(laser, rtc);
+                        break;
                 }
-                Console.WriteLine($"Processing time = {timer.ElapsedMilliseconds/1000.0:F3}s");     
             } while (true);
 
+            if (rtc.CtlGetStatus(RtcStatus.Busy))
+            {
+                rtc.CtlAbort();
+                rtc.CtlBusyWait();
+            }
             rtc.Dispose();
         }
         /// <summary>
@@ -123,6 +131,10 @@ namespace SpiralLab.Sirius
         /// <param name="radius"></param>
         private static bool DrawCircle(ILaser laser, IRtc rtc, float radius)
         {
+            if (rtc.CtlGetStatus(RtcStatus.Busy))
+                return false;
+            Console.WriteLine("WARNING !!! LASER IS BUSY ... Draw Circle");
+            timer = Stopwatch.StartNew();
             bool success = true;
             success &= rtc.ListBegin(laser);
             success &= rtc.ListJump(new Vector2(radius, 0));
@@ -130,6 +142,7 @@ namespace SpiralLab.Sirius
             success &= rtc.ListEnd();
             if (success)
                 success &= rtc.ListExecute(true);
+            Console.WriteLine($"Processing time = {timer.ElapsedMilliseconds / 1000.0:F3}s");
             return success;
         }
 
@@ -144,6 +157,10 @@ namespace SpiralLab.Sirius
         /// <param name="y2"></param>
         private static bool DrawLine(ILaser laser, IRtc rtc, float x1, float y1, float x2, float y2)
         {
+            if (rtc.CtlGetStatus(RtcStatus.Busy))
+                return false;
+            Console.WriteLine("WARNING !!! LASER IS BUSY ... DrawLine");
+            timer = Stopwatch.StartNew();
             bool success = true;
             success &= rtc.ListBegin(laser);
             success &= rtc.ListJump(new Vector2(x1, y1));
@@ -151,6 +168,7 @@ namespace SpiralLab.Sirius
             success &= rtc.ListEnd();
             if (success)
                 success &= rtc.ListExecute(true);
+            Console.WriteLine($"Processing time = {timer.ElapsedMilliseconds / 1000.0:F3}s");
             return success;
         }
         /// <summary>
@@ -162,6 +180,10 @@ namespace SpiralLab.Sirius
         /// <param name="height"></param>
         private static bool DrawRectangle(ILaser laser, IRtc rtc, float width, float height)
         {
+            if (rtc.CtlGetStatus(RtcStatus.Busy))
+                return false;
+            Console.WriteLine("WARNING !!! LASER IS BUSY ... DrawRectangle");
+            timer = Stopwatch.StartNew();
             bool success = true;
             success &= rtc.ListBegin(laser);
             success &= rtc.ListJump(new Vector2(-width / 2, height / 2));
@@ -172,6 +194,7 @@ namespace SpiralLab.Sirius
             success &= rtc.ListEnd();
             if (success)
                 success &= rtc.ListExecute(true);
+            Console.WriteLine($"Processing time = {timer.ElapsedMilliseconds / 1000.0:F3}s");
             return success;
         }
         /// <summary>
@@ -183,6 +206,10 @@ namespace SpiralLab.Sirius
         /// <param name="durationMsec"></param>
         private static bool DrawCircleWithDots(ILaser laser, IRtc rtc, float radius, float durationMsec)
         {
+            if (rtc.CtlGetStatus(RtcStatus.Busy))
+                return false;
+            Console.WriteLine("WARNING !!! LASER IS BUSY ... DrawCircleWithDots");
+            timer = Stopwatch.StartNew();
             bool success = true;
             success &= rtc.ListBegin(laser);
             for (float angle =0; angle<360; angle+=1)
@@ -198,6 +225,7 @@ namespace SpiralLab.Sirius
             success &= rtc.ListEnd();
             if (success)
                 success &= rtc.ListExecute(true);
+            Console.WriteLine($"Processing time = {timer.ElapsedMilliseconds / 1000.0:F3}s");
             return success;
         }
         /// <summary>
@@ -209,6 +237,10 @@ namespace SpiralLab.Sirius
         /// <param name="gap"></param>
         private static bool DrawSquareAreaWithPixels(ILaser laser, IRtc rtc, float size, float gap)
         {
+            if (rtc.CtlGetStatus(RtcStatus.Busy))
+                return false;
+            Console.WriteLine("WARNING !!! LASER IS BUSY ... DrawSquareAreaWithPixels");
+            timer = Stopwatch.StartNew();
             // pixel operation 은 IRtcExtension 인터페이스에서 제공
             var rtcExt = rtc as IRtcExtension;
             if (null == rtcExt)
@@ -230,7 +262,73 @@ namespace SpiralLab.Sirius
             success &= rtc.ListEnd();
             if (success)
                 success &= rtc.ListExecute(true);
+            Console.WriteLine($"Processing time = {timer.ElapsedMilliseconds / 1000.0:F3}s");
             return success;
+        }
+
+        static Thread thread;
+        static ILaser laser;
+        static IRtc rtc;
+        private static void DrawTooHeavyAndSlowJob(ILaser laser, IRtc rtc)
+        {
+            if (rtc.CtlGetStatus(RtcStatus.Busy))
+            {
+                Console.WriteLine("Processing are working already !");
+                return;
+            }
+
+            Program.laser = laser;
+            Program.rtc = rtc;
+            Program.thread = new Thread(DoHeavyWork);
+            Program.thread.Start();
+        }
+        private static void DoHeavyWork()
+        {
+            bool success = true;
+            float width = 1;
+            float height = 1;
+
+            Console.WriteLine("WARNING !!! LASER IS BUSY... DoHeavyWork thread");
+            timer = Stopwatch.StartNew();
+            success &= rtc.ListBegin(laser, ListType.Auto); //대량의 데이타를 처리하기 위해 auto 리스트 버퍼 사용
+            success &= rtc.ListJump(new Vector2(-width / 2, height / 2));
+            for (int i = 0; i < 1000 * 1000 * 10; i++)
+            {
+                success &= rtc.ListMark(new Vector2(width / 2, height / 2));
+                success &= rtc.ListMark(new Vector2(width / 2, -height / 2));
+                success &= rtc.ListMark(new Vector2(-width / 2, -height / 2));
+                success &= rtc.ListMark(new Vector2(-width / 2, height / 2));
+                if (!success)
+                    break;
+            }
+            if (success)
+            {
+                success &= rtc.ListEnd();
+                success &= rtc.ListExecute();
+            }
+            if (success)
+                Console.WriteLine("Success to mark by DoHeavyWork thread");
+            else
+                Console.WriteLine("Fail to mark by DoHeavyWork thread !");
+            Console.WriteLine($"Processing time = {timer.ElapsedMilliseconds / 1000.0:F3}s");
+        }
+
+        private static void StopMarkAndReset(ILaser laser, IRtc rtc)
+        {
+            Console.WriteLine("Trying to abort ...");
+
+            //abort to mark
+            rtc.CtlAbort();
+
+            //wait for rtc busy off
+            rtc.CtlBusyWait();
+
+            //wait for thread has done
+            Program.thread?.Join();
+            Program.thread = null;
+
+            //reset rtc's status
+            rtc.CtlReset();
         }
     }
 }
