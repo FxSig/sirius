838b24253e18d40a7c97752bc0a1d265a4058de5
sepwind <sepwind@naver.com>
2021-09-02 15:42:00 +0900

    updated) sirius 문서

--- sirius.cs
+++ sirius.cs
@@ -22,19 +22,19 @@
  * 
  * 
  * 
- * 
- * 
- * 
- *        IDocument                                                                                          ILaser                     IRtc  ---  {spirallab.sirius.rtc.dll}
- *   (문서 인터페이스)                                                                              (레이저소스 인터페이스)       (RTC 인터페이스)
- *          /|\                                                                                               /|\                        /|\
- *           |                                                                                                 |                          |   [IRtcExtension] [IRtc3D] [IRtcDualHead] [IRtcMOTF]
- *           |                                                                                                 |                          | (RTC 확장 인터페이스, 3D 가공 인터페이스, 듀얼헤드 인터페이스, MOTF 인터페이스)
- *           |                                                                                                 |                          |
- *           |                                                                                                 |                          |
- *           |                                                                                                 |                          |
- *    DocumentDefault ----------------- Action (Undo/Redo 를 처리하기 위한 액션 담당)                    YourCustomLaser         Rtc4, Rtc5, Rtc6, RtcSyncAxis...
- *    (기본 문서)                       Blocks (AutoCad 의 Block 집합 (BlockInsert 용))                   (레이저 소스)           (스캐너 제어 카드)
+ *                                                                                                           ILaser        -----------  {spirallab.sirius.rtc.dll} --------------- IMotor/IDInput/IDOutput
+ *    {spirallab.sirius.dll}                                                                          (레이저소스 인터페이스)                       |
+ *                                                                                                            /|\ 
+ *        IDocument                                                              [IShutter] [IPowerControl]    |                                   IRtc  
+ *   (문서 인터페이스)                                                                                         |                              (RTC 인터페이스)
+ *          /|\                                                                                                |                                   /|\
+ *           |                                                                             IPGYLPTypeE         |  IPGYLPTypeD                       |   [IRtcExtension] [IRtc3D] [IRtcDualHead] [IRtcMOTF] [ISyncAxis]
+ *           |                                                                                  JPTTypeE       |   CoherentAviaLX                   | (RTC 확장 인터페이스, 3D 가공 인터페이스, 듀얼헤드 인터페이스, MOTF 인터페이스)
+ *           |                                                                           PhotonicsIndustryDX   |   AdvancedOptoWaveFotia            |
+ *           |                                                                                SPIG4            |                                    |
+ *           |                                                                                                 |                                    |
+ *    DocumentDefault ----------------- Action (Undo/Redo 를 처리하기 위한 액션 담당)                          |                       Rtc4, Rtc5, Rtc6, RtcSyncAxis...
+ *    (기본 문서)                       Blocks (AutoCad 의 Block 집합 (BlockInsert 용))                   (레이저 소스)                       (스캐너 제어)
  *                                      Layers (AutoCad 의 Layer 집합) ---------------------------|
  *                             |------- View  (뷰 : 화면)                                         |
  *                             |                                                                  |
@@ -46,8 +46,8 @@
  *         IView  <------------|                          IMarker                               Layer ----------- IEntity   
  *     (뷰 인터페이스)                                (마커 인터페이스)                        (레이어)     (엔티티 인터페이스)
  *          /|\                                             /|\                                                     /|\    
- *           |                                               |                                                       |      [IMarkerable] [ICloneable] [IDrawable] [IExplodable] [IHatchable]
- *           |                                               |                                                       |  (마킹 인터페이스 복제 인터페이스 그리기 인터페이스 분해 인터페이스 해치 인터페이스)
+ *           |                                               |                                                       |      [IMarkerable] [ICloneable] [IDrawable] [IExplodable] [IHatchable] [IScriptable]
+ *           |                                               |                                                       |                      (마킹 / 복제 / 그리기 / 분해 / 해치 / 스크립트)
  *           |                                               |                                                       |
  *           |                                               |                                                       |
  *           |                                               |                                                       |
