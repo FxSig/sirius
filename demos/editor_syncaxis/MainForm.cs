8a38e07e5112d7cd593748fb5fc3536c5a4e3c3a
labspiral <labspiral@gmail.com>
2023-01-04 10:32:08 +0900

    fixed) improved demos\editor_syncaxis demo (데모 코드 향상)

--- demos/editor_syncaxis/MainForm.cs
+++ demos/editor_syncaxis/MainForm.cs
@@ -67,21 +67,46 @@ namespace SpiralLab.Sirius
 
         ILaser Laser;
 
+        /// <summary>
+        /// 레이저 최대 출력 (Watt)
+        /// </summary>
+        public float LaserMaxPowerWatt = 5.0f;
+        /// <summary>
+        /// 마커 인터페이스
+        /// </summary>
+        public IMarker Marker { get; set; }
+        /// <summary>
+        /// 모터 집합
+        /// </summary>
+        public IMotor[] Motors { get; set; }
+        /// <summary>
+        /// 파워메터 인터페이스
+        /// </summary>
+        public IPowerMeter PowerMeter { get; set; }
+        /// <summary>
+        /// 파워맵 인터페이스
+        /// </summary>
+        public IPowerMap PowerMap { get; set; }
+
+
+
         public MainForm()
         {
             InitializeComponent();
 
+            // initialize core engine
             SpiralLab.Core.Initialize();
-            
+            // enable pens function
+            siriusEditorForm1.EnablePens = true;
+
+            // create document
             // 신규 문서 생성
             var doc = new DocumentDefault();
+            // assign document into editor
+            // 문서 지정
             siriusEditorForm1.Document = doc;
-            if (!siriusEditorForm1.EnablePens)
-            {
-                // 기본 펜 생성후 문서에 추가
-                var pen = new PenDefault();
-                doc.Action.ActEntityAdd(pen);
-            }
+
+            // assign document source changed event handler
             // 내부 데이타(IDocument) 가 변경될경우 이를 이벤트 통지를 받는 핸들러 등록
             siriusEditorForm1.OnDocumentSourceChanged += SiriusEditorForm1_OnDocumentSourceChanged;
 
@@ -134,12 +159,16 @@ namespace SpiralLab.Sirius
             laser.Rtc = rtc;
             // initialize laser source
             laser.Initialize();
-            // set basic power output to 2W
-            laser.CtlPower(2);
+            // 10% output 
+            laser.CtlPower(LaserMaxPowerWatt * 0.1f);
             #endregion
 
             #region 마커 지정
             var marker = new MarkerDefault(0, "SyncAxis Marker");
+            marker.OnStarted += Marker_OnStarted;
+            marker.OnProgress += Marker_OnProgress;
+            marker.OnFinished += Marker_OnFinished;
+            marker.OnFailed += Marker_OnFailed;
             #endregion
 
             #region RTC 확장 IO 
@@ -150,15 +179,53 @@ namespace SpiralLab.Sirius
             //this.RtcExt2DOutput = new RtcDOutputExt2(rtc, 0, "DIN RTC EXT2");
             //this.RtcExt2DOutput.Initialize();
             //this.siriusEditorForm1.RtcExtension1Input = this.RtcExt1DInput;
-            this.siriusEditorForm1.RtcExtension1Output = this.RtcExt1DOutput;
             //this.siriusEditorForm1.RtcExtension2Output = this.RtcExt2DOutput;
             #endregion
 
+            #region PowerMeter
+            // 파워메터
+            var powerMeter = new PowerMeterVirtual(0, "Virtual", laser.MaxPowerWatt);
+            //var powerMeter = new PowerMeterOphir(0, "OphirJuno", "3040875");
+            //var powerMeter = new PowerMeterCoherentPowerMax(0, "CoherentPM", 1);
+            //var powerMeter = new PowerMeterThorLabsPMSeries(0, "PM100USB", "SERIALNO");
+            powerMeter.Initialize();
+            #endregion
+
+            #region Powermap
+            var powerMap = new PowerMapDefault(0, "Virtual", "Watt");
+            //var powerMapFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "powermap", "default.map");
+            //PowerMapSerializer.Open(powerMap, powerMapFile);
+            laser.PowerMap = powerMap;
+            #endregion        
+
             this.Rtc = rtc;
             this.Laser = laser;
+            this.Marker = marker;
+            this.PowerMeter = powerMeter;
+            this.PowerMap = powerMap;
+
             this.siriusEditorForm1.Rtc = rtc;
             this.siriusEditorForm1.Laser = laser;
             this.siriusEditorForm1.Marker = marker;
+            this.siriusEditorForm1.RtcExtension1Output = this.RtcExt1DOutput;
+            this.siriusEditorForm1.PowerMeter = powerMeter;
+            this.siriusEditorForm1.PowerMap = powerMap;
+        }
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Marker?.Stop();
+
+            Marker.OnStarted -= Marker_OnStarted;
+            Marker.OnProgress -= Marker_OnProgress;
+            Marker.OnFinished -= Marker_OnFinished;
+            Marker.OnFailed -= Marker_OnFailed;
+
+            PowerMeter?.Dispose();
+            Laser?.Dispose();
+            siriusEditorForm1.RtcExtension1Input?.Dispose();
+            siriusEditorForm1.RtcExtension1Output?.Dispose();
+            siriusEditorForm1.RtcExtension2Output?.Dispose();
+            Rtc?.Dispose();
         }
 
         private void SiriusEditorForm1_OnDocumentSourceChanged(object sender, IDocument doc)
@@ -166,15 +233,141 @@ namespace SpiralLab.Sirius
             siriusEditorForm1.Document = doc;
         }
 
-        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        private void Marker_OnStarted(IMarker sender, IMarkerArg markerArg)
         {
-            this.siriusEditorForm1.Marker.Stop();
-            Laser?.Dispose();
-            Rtc?.Dispose();
+            Logger.Log(Logger.Type.Info, $"{sender.Name} marker has started");
+        }
+        private void Marker_OnProgress(IMarker sender, IMarkerArg markerArg)
+        {
+            Logger.Log(Logger.Type.Info, $"{sender.Name} marker has {markerArg.Progress} progressing...");
+        }
+        private void Marker_OnFailed(IMarker sender, IMarkerArg markerArg)
+        {
+            Logger.Log(Logger.Type.Error, $"{sender.Name} marker has failed");
+        }
+        private void Marker_OnFinished(IMarker sender, IMarkerArg markerArg)
+        {
+            var timeSpan = markerArg.EndTime - markerArg.StartTime;
+            Logger.Log(Logger.Type.Info, $"{sender.Name} marker has finished. {timeSpan.TotalSeconds:F3}s");
+        }
 
-            //this.siriusEditorForm1.Rtc = null;
-            //this.siriusEditorForm1.Laser = null;
-            //this.siriusEditorForm1.Marker = null;
+        /// <summary>
+        /// 레시피 변경
+        /// </summary>
+        /// <param name="siriusFileName"></param>
+        /// <returns></returns>
+        public bool Open(string siriusFileName)
+        {
+            if (this.IsBusy())
+            {
+                Logger.Log(Logger.Type.Info, $"system is busy. fail to change target recipe");
+                return false;
+            }
+            try
+            {
+                var doc = DocumentSerializer.OpenSirius(siriusFileName);
+                siriusEditorForm1.Document = doc;
+                //if you want to download recipe file by automatically
+                bool success = this.Ready();
+                if (success)
+                    Logger.Log(Logger.Type.Info, $"success to change target recipe by {siriusFileName}");
+                else
+                    Logger.Log(Logger.Type.Error, $"system is busy. fail to change target recipe");
+                return success;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// 가공 데이타가 다운로드 된다
+        /// </summary>
+        /// <returns></returns>
+        public bool Ready()
+        {
+            if (this.IsBusy())
+                return false;
+            var arg = new MarkerArgDefault()
+            {
+                Document = siriusEditorForm1.Document,
+                Rtc = Rtc,
+                Laser = Laser,
+                RtcListType = ListType.Auto,
+                MarkTargets = MarkTargets.All,
+            };
+            arg.ViewTargets.Add(siriusEditorForm1.View);
+            arg.Offsets.Clear();
+            return Marker.Ready(arg);
+        }
+        /// <summary>
+        /// 가공 시작
+        /// </summary>
+        /// <returns></returns>
+        public bool Start()
+        {
+            if (this.IsBusy())
+                return false;
+
+            var doc = siriusEditorForm1.Document;
+            Marker.MarkerArg.Offsets.Clear();
+            return Marker.Start();
+        }
+        /// <summary>
+        /// 가공 중지
+        /// </summary>
+        /// <returns></returns>
+        public bool Abort()
+        {
+            bool success = true;
+            success &= Rtc.CtlAbort();
+            success &= Laser.CtlAbort();
+            success &= Marker.Stop();
+            return success;
+        }
+        /// <summary>
+        /// 에러 리셋
+        /// </summary>
+        /// <returns></returns>
+        public bool Reset()
+        {
+            bool success = true;
+            success &= Rtc.CtlReset();
+            success &= Laser.CtlReset();
+            success &= Marker.Reset();
+            return success;
+        }
+        /// <summary>
+        /// 가공 준비 상태
+        /// </summary>
+        /// <returns></returns>
+        public bool IsReady()
+        {
+            return Marker.IsReady;
+        }
+        /// <summary>
+        /// 가공중 여부
+        /// </summary>
+        /// <returns></returns>
+        public bool IsBusy()
+        {
+            bool busy = false;
+            busy |= Rtc.CtlGetStatus(RtcStatus.Busy);
+            busy |= Laser.IsBusy;
+            busy |= Marker.IsBusy;
+            return busy;
+        }
+        /// <summary>
+        /// 에러 여부
+        /// </summary>
+        /// <returns></returns>
+        public bool IsError()
+        {
+            bool error = false;
+            error |= !Rtc.CtlGetStatus(RtcStatus.NoError);
+            error |= Laser.IsError;
+            error |= Marker.IsError;
+            return error;
         }
     }
 }
